14c40c7bbe76da786bb89977f423886a7ed4ef13
Gopal sharma <sharmagopal7316@gmail.com>
2024-07-15 19:42:44 +0530

    first commit

---
+++ practice set/some math functions/log.py
@@ -0,0 +1,15 @@
+#Formate: log(x,base)
+# it return the log arithmetic of x to the given base.
+#Note: here value of x and base should be positive . otherwise it return the an error
+import math
+x=2
+y=3
+print(math.log(x,y))
+
+x1=2.5
+y1=3.45
+print(math.log(x1,y1))
+
+x1=5.55
+y1=9.45
+print(math.log(x1,y1))
---
+++ practice set/some math functions/sqrt.py
@@ -0,0 +1,15 @@
+# sqrt returns the square root of x.
+
+#NOTE:value of x should be positive.otherwise it will be throw an error.
+import math
+x=49
+print(math.sqrt(x))
+
+x1=36
+print(math.sqrt(x1))
+
+x2=53.499
+print(math.sqrt(x2))
+
+x3=88
+print(math.sqrt(x3))
