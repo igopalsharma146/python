8cb33f3d7072a89f59055adb52a611f834942256
Gopal sharma <sharmagopal7316@gmail.com>
2024-07-21 00:33:26 +0530

    second commit

--- class/convert grams to kg.py
+++ class/convert grams to kg.py
@@ -1,5 +1,12 @@
-gram=int(input("enter the value of gram: "))
-kg=gram//1000
-rem=gram%1000
-s="{0} kg {1} grams"
-print(s.format(kg,rem))
+# Function to convert grams to kilograms
+def grams_to_kilograms(grams):
+    return grams / 1000
+
+# Input from the user
+grams = float(input("Please enter the weight in grams: "))
+
+# Conversion
+kilograms = grams_to_kilograms(grams)
+
+# Output the result
+print(f"{grams} grams is equal to {kilograms} kilograms")
