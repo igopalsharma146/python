f90c415d41b66f098e9a0ae5f122260630812305
Gopal sharma <sharmagopal7316@gmail.com>
2024-07-20 22:40:11 +0530

    first commit

---
+++ class/Create an full array.py
@@ -0,0 +1,4 @@
+#Create an full array
+import numpy as np
+array=np.full((3,4),5)
+print(array)
---
+++ class/create an empty array.py
@@ -0,0 +1,11 @@
+#Create an empty array
+import numpy as np
+array=np.empty((3,4))
+print(array)
+
+print("\n-----------------------------------------------------------------------------------------------\n")
+
+#By using list
+l1=[]
+arr=np.array(l1)
+print(arr)
