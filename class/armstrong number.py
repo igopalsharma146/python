521100c479188f483f682056ee1f97b2d398a0d8
Gopal sharma <sharmagopal7316@gmail.com>
2024-07-21 00:23:40 +0530

    second commit

--- class/armstrong number.py
+++ class/armstrong number.py
@@ -1,12 +1,19 @@
+def length(n):
+    res=0
+    while(n>0):
+        n=n//10
+        res=res+1
+    return res
+
 n=eval(input("Enter a number."))
-sum=0 
 res=0
 temp=n
+length=length(n)
 while(n>0):
     rem=n%10
-    res=res+rem**3
+    res=res+rem**length
     n=n//10
 if(temp==res):
     print(temp,"is a armstrong number.")
 else:
-    print(temp,"is not a prime number.")
+    print(temp,"is not a armstrong number.")
